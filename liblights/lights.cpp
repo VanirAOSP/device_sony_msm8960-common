5153601cd55a3079f86f31bf982b0be6b9ae36cd
Mrcl1450 <marcelo-fil@outlook.com>
2015-05-24 22:13:00 +0100

    Revert "Initial TriLed"
    This reverts commit 2ac077c39466b1f162de8cee562bdff88c9a795d.

--- liblights/lights.cpp
+++ liblights/lights.cpp
@@ -37,22 +37,10 @@
 
 #include "sony_lights.h"
 
-//#include <stdint.h>
-//#include <string.h>
-//#include <unistd.h>
-//#include <errno.h>
-//#include <pthread.h>
-//#include <sys/ioctl.h>
-//#include <sys/types.h>
-
-static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 static android::Mutex gLightWrapperLock;
 static hw_module_t *gVendorModule = 0;
-
-static struct light_state_t g_notification;
-static struct light_state_t g_battery;
 
 static int open_lights(const hw_module_t* module, const char* name,
                        hw_device_t** device);
@@ -177,11 +165,6 @@ static int write_string (const char *path, const char *value) {
     return written == -1 ? -errno : 0;
 }
 
-/* Color tools */
-static int is_lit (struct light_state_t const* state) {
-	return state->color & 0x00ffffff;
-}
-
 static int rgb_to_brightness (struct light_state_t const* state) {
     int color = state->color & 0x00ffffff;
     return ((77*((color>>16)&0x00ff))
@@ -253,67 +236,6 @@ static int lights_set_light_backlight (struct light_device_t *dev, struct light_
     pthread_mutex_unlock(&g_lock);
 
     return err;
-}
-
-static int set_light_buttons (struct light_device_t *dev, struct light_state_t const* state) {
-	return 0;
-}
-
-static void set_shared_light_locked (struct light_device_t *dev, struct light_state_t *state) {
-	int r, g, b, i;
-
-	r = (state->color >> 16) & 0xFF;
-	g = (state->color >> 8) & 0xFF;
-	b = (state->color) & 0xFF;
-
-	}
-
-	write_int (RED1_LED_FILE, r);
-	write_int (RED2_LED_FILE, r);
-	write_int (RED3_LED_FILE, r);
-	write_int (RED1C_LED_FILE, r);
-	write_int (RED2C_LED_FILE, r);
-	write_int (RED3C_LED_FILE, r);
-	write_int (GREEN1_LED_FILE, g);
-	write_int (GREEN2_LED_FILE, g);
-	write_int (GREEN3_LED_FILE, g);
-	write_int (GREEN1C_LED_FILE, g);
-	write_int (GREEN2C_LED_FILE, g);
-	write_int (GREEN3C_LED_FILE, g);
-	write_int (BLUE1_LED_FILE, b);
-	write_int (BLUE2_LED_FILE, b);
-	write_int (BLUE3_LED_FILE, b);
-	write_int (BLUE1C_LED_FILE, b);
-	write_int (BLUE2C_LED_FILE, b);
-	write_int (BLUE3C_LED_FILE, b);
-}
-
-static void handle_shared_battery_locked (struct light_device_t *dev) {
-	if (is_lit (&g_notification))
-		set_shared_light_locked (dev, &g_notification);
-	else
-		set_shared_light_locked (dev, &g_battery);
-}
-
-static int set_light_battery (struct light_device_t *dev, struct light_state_t const* state) {
-	pthread_mutex_lock (&g_lock);
-	g_battery = *state;
-	handle_shared_battery_locked(dev);
-	pthread_mutex_unlock (&g_lock);
-	return 0;
-}
-
-static int set_light_notifications (struct light_device_t *dev, struct light_state_t const* state) {
-	pthread_mutex_lock (&g_lock);
-	g_notification = *state;
-	handle_shared_battery_locked(dev);
-	pthread_mutex_unlock (&g_lock);
-	return 0;
-}
-
-/* Initializations */
-void init_globals () {
-	pthread_mutex_init (&g_lock, NULL);
 }
 
 static int open_lights(const hw_module_t* module, const char* name,
