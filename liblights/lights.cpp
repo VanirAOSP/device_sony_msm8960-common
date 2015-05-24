fa17f2d697bc869a89595984f618f399a5285f1b
Mrcl1450 <marcelo-fil@outlook.com>
2015-05-24 11:54:54 +0100

    TriLed (2)

--- liblights/lights.cpp
+++ liblights/lights.cpp
@@ -135,6 +135,24 @@ done:
     return ret;
 }
 
+static int read_int (const char *path) {
+	int fd;
+	char buffer[20];
+	
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+	  return -1;
+	}
+	
+	int cnt = read(fd, buffer, 20);
+	close(fd);
+	
+	if(cnt > 0) {
+	  return atoi(buffer);
+	}
+	return -1;
+}
+
 static int write_int (const char *path, int value) {
     int fd;
     static int already_warned = 0;
@@ -260,7 +278,7 @@ static int set_light_buttons (struct light_device_t *dev, struct light_state_t c
 }
 
 static void set_shared_light_locked (struct light_device_t *dev, struct light_state_t *state) {
-	int r, g, b, i;
+	int r, g, b;
 
 	r = (state->color >> 16) & 0xFF;
 	g = (state->color >> 8) & 0xFF;
@@ -287,10 +305,11 @@ static void set_shared_light_locked (struct light_device_t *dev, struct light_st
 }
 
 static void handle_shared_battery_locked (struct light_device_t *dev) {
-	if (is_lit (&g_notification))
+	if (is_lit (&g_notification)) {
 		set_shared_light_locked (dev, &g_notification);
-	else
+	} else {
 		set_shared_light_locked (dev, &g_battery);
+	}
 }
 
 static int set_light_battery (struct light_device_t *dev, struct light_state_t const* state) {
@@ -314,8 +333,16 @@ void init_globals () {
 	pthread_mutex_init (&g_lock, NULL);
 }
 
+/* Glueing boilerplate */
+static int close_lights (struct light_device_t *dev) {
+	if (dev)
+		free(dev);
+
+	return 0;
+}
+
 static int open_lights(const hw_module_t* module, const char* name,
-                       hw_device_t** device)
+						struct hw_device_t** device) {
 {
     int rv = 0;
     int (*set_light)(struct light_device_t* dev,
@@ -360,12 +387,24 @@ static int open_lights(const hw_module_t* module, const char* name,
         else
             return -EINVAL;
 
+		pthread_once (&g_init, init_globals);
+		struct light_device_t *dev = malloc(sizeof (struct light_device_t));
+		memset(dev, 0, sizeof(*dev));
+		
+		dev->common.tag 	= HARDWARE_DEVICE_TAG;
+		dev->common.version = 0;
+		dev->common.module 	= (struct hw_module_t*)module;
+		dev->common.close 	= (int (*)(struct hw_device_t*))close_lights;
+		dev->set_light 		= set_light;
+	
         light_device->base.common.tag     = HARDWARE_DEVICE_TAG;
         light_device->base.common.version = 0;
         light_device->base.common.module  = (hw_module_t *)(module);
         light_device->base.common.close   = lights_device_close;
         light_device->base.set_light      = set_light;
-
+		
+		*device = (struct hw_device_t*)dev;
+		
         *device = (struct hw_device_t*)light_device;
     }
 
@@ -380,3 +419,18 @@ fail:
 
     return rv;
 }
+
+static struct hw_module_methods_t lights_module_methods = {
+	.open = open_lights,
+};
+
+
+struct hw_module_t HAL_MODULE_INFO_SYM = {
+	.tag = HARDWARE_MODULE_TAG,
+	.version_major = 1,
+	.version_minor = 0,
+	.id = LIGHTS_HARDWARE_MODULE_ID,
+	.name = "Sony lights module",
+	.author = "Diogo Ferreira <defer@cyanogenmod.com>, Andreas Makris <Andreas.Makris@gmail.com>",
+	.methods = &lights_module_methods,
+};
