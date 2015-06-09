200cea969e0ee3ff1eaf57ae19331161ae764c85
Adrian DC <radian.dc@gmail.com>
2015-06-09 18:42:48 +0200

    LibLights & LibAls - Huashan libraries cleanup

--- libals/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := device/sony/huashan/include
-LOCAL_SRC_FILES := als.c
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE := libals
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
